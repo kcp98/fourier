3bf93d965b2d7c5aa2013548abc3420b1d79792d
kcp98 <bkcp@pm.me>
2024-05-19 21:02:39 +0200

    spent a whole day making figures... no time for refactor

--- benchmark/main.py
+++ benchmark/main.py
@@ -13,44 +13,96 @@ def regression(x : np.array, y : np.array):
 
 def make_plot(fname : str, title : str):
     array = np.loadtxt(f"benchmark/data/{fname}.txt")
-    x, y = array[:,0], array[:,1] / 1000 # back to seconds...
+    x, y = array[:,0], array[:,1]
     fig, (ax1, ax2, ax3) = plt.subplots(1,3)
-    fig.suptitle(f"Running time for {title}")
-    fig.set_size_inches(15, 5)
+    fig.suptitle(f"Running time for {title}", fontsize=16)
+    fig.set_size_inches(18, 6)
 
-    if True:
+    if True: # O(nlgn)
         fnlgn, r2 = regression(x * np.log(x), y)
         ax1.plot(x, fnlgn, '-', color="orchid")
-        ax1.plot(x,     y, '.', color="black")
-        ax1.annotate(f"$R^2 = {r2[0]:0.6f}$", (min(x), max(y)))
-        ax1.set_title(r"$O(n \lg n)$")
-        ax1.set_ylabel('time (seconds)')
-        ax1.set_xlabel('input size')
+        ax1.plot(x,     y, '.', color="gray")
+        ax1.annotate(f"$R^2 = {r2[0]:0.5f}$", (max(x)/2, 0), fontsize=14)
+        ax1.set_title(r"$O(n \lg n)$", fontsize=16)
+        ax1.set_ylabel('time (seconds)', fontsize=16)
+        ax1.set_xlabel('input size', fontsize=16)
         ax1.set_xticks(x[-4::])
         ax1.set_yticks(y[-4::])
+        ax1.set_xticklabels([f"2^{j:.0f}" for j in np.log2(x[-4::])])
 
-    if True:
+    if True: # O(n^2)
         fnn, r2 = regression(x * x, y)
         ax2.plot(x, fnn, '-', color="orchid")
-        ax2.plot(x,   y, '.', color="black")
-        ax2.annotate(f"$R^2 = {r2[0]:0.6f}$", (min(x), max(y)))
-        ax2.set_title(r"$O(n^2)$")
+        ax2.plot(x,   y, '.', color="gray")
+        ax2.annotate(f"$R^2 = {r2[0]:0.5f}$", (max(x)/2, 0), fontsize=14)
+        ax2.set_title(r"$O(n^2)$", fontsize=16)
         ax2.set_xticks([])
         ax2.set_yticks([])
  
-    if True:
+    if True: # O(n)
         fn, r2 = regression(x, y)
         ax3.plot(x, fn, '-', color="orchid")
-        ax3.plot(x,  y, '.', color="black")
-        ax3.annotate(f"$R^2 = {r2[0]:0.6f}$", (min(x), max(y)))
-        ax3.set_title(r"$O(n)$")
+        ax3.plot(x,  y, '.', color="gray")
+        ax3.annotate(f"$R^2 = {r2[0]:0.6f}$", (max(x)/2, 0), fontsize=14)
+        ax3.set_title(r"$O(n)$", fontsize=16)
         ax3.set_xticks([])
         ax3.set_yticks([])
 
     fig.savefig(f"benchmark/images/{fname}.png")
     fig.clear()
 
+def make_reference_plots():
+    fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+    fig.suptitle(f"Reference frame", fontsize=16)
+    fig.set_size_inches(18, 6)
+
+    if True: # long mul
+        array = np.loadtxt(f"benchmark/data/lmul.txt")
+        x, y = array[:,0], array[:,1]
+        f, r2 = regression(x * x, y)
+        ax1.plot(x, f, '-', color="darkorchid")
+        ax1.plot(x, y, '.', color="black")
+        ax1.annotate(f"$R^2 = {r2[0]:0.5f}$", (max(x)/2, 0), fontsize=14)
+        ax1.set_title(r"$O(n^2)$ for long multiplication", fontsize=14)
+        ax1.set_ylabel('time (seconds)', fontsize=14)
+        ax1.set_xlabel('input size', fontsize=14)
+        ax1.set_xticks(x[-4::])
+        ax1.set_yticks(y[-4::])
+        ax1.set_xticklabels([f"2^{j:.0f}" for j in np.log2(x[-4::])])
+
+    if True: # numpy
+        array = np.loadtxt(f"benchmark/data/nmul.txt")
+        x, y = array[:,0], array[:,1]
+        f, r2 = regression(x * x, y)
+        ax2.plot(x, f, '-', color="deepskyblue")
+        ax2.plot(x,  y, '.', color="black")
+        ax2.annotate(f"$R^2 = {r2[0]:0.6f}$", (max(x)/2, 0), fontsize=14)
+        ax2.set_title(r"$O(n^2)$ for numpys polymul", fontsize=14)
+        ax2.set_xticks(x[-4::])
+        ax2.set_yticks(y[-4::])
+        ax2.set_xticklabels([f"2^{j:.0f}" for j in np.log2(x[-4::])])
+    
+    if True: # numpy vs. fmul
+        array = np.loadtxt(f"benchmark/data/nmul.txt")
+        x, y = array[:,0][2:-1], array[:,1][2:-1]
+        ax3.plot(x, y, '-', color="deepskyblue")
+        ax3.plot(x, y, '.', color="black")
+        
+        array = np.loadtxt(f"benchmark/data/fmul.txt")
+        x, y = array[:,0][:-3], array[:,1][:-3]
+        ax3.plot(x, y, '-', color="orchid")
+        ax3.plot(x, y, '.', color="gray")
+
+        ax3.set_title(r"numpy vs. fmul", fontsize=14)
+        ax3.set_xticks(x[-4::])
+        ax3.set_yticks(y[-4::])
+
+        ax3.set_xticklabels([f"2^{j:.0f}" for j in np.log2(x[-4::])])
+
+    fig.savefig(f"benchmark/images/ref.png")
+    fig.clear()
+
+
 if __name__ == "__main__":
     make_plot("fmul", "fft multiplication")
-    make_plot("rmul", "long multiplication")
-    make_plot("nmul", "numpys polymul")
+    make_reference_plots()
--- code/analysis.py
+++ code/analysis.py
@@ -11,12 +11,12 @@ def timestmt(n : int, stmt : str):
     )
     print(f"n = {n}")
     times = timeit.repeat(setup=SETUP_CODE,stmt=stmt,number=10)
-    return min(times) * 1000 # miliseconds
+    return min(times)
 
 def benchmark(func, fname, limit):
     """ Save a table of running time (in ms) for range of inputs
     """
-    ns = 1 << np.arange(6,limit)
+    ns = 1 << np.arange(limit-13,limit)
     ts = np.vectorize(func)(ns)
     r  = np.vstack((ns,ts))
     np.savetxt(f"benchmark/data/{fname}.txt",  r.T)
@@ -26,6 +26,6 @@ lmul = lambda n : timestmt(n, "lmul(x,y)")
 nmul = lambda n : timestmt(n, "np.polynomial.polynomial.polymul(x,y)")
 
 if __name__ == "__main__":
-    benchmark(fmul, "fmul", limit = 16)
-    benchmark(lmul, "lmul", limit = 13)
-    benchmark(nmul, "nmul", limit = 16)
+    benchmark(fmul, "fmul", limit = 20) # 886.32s user 11.27s system 100% cpu 14:56.64 total
+    benchmark(lmul, "lmul", limit = 14) # don't recall
+    benchmark(nmul, "nmul", limit = 18) # 532.80s user 1.48s system 100% cpu 8:52.57 total

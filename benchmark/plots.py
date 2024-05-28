cd135695e564c54b7a82950c2ca99ba6fcd52583
kcp98 <bkcp@pm.me>
2024-05-28 23:25:05 +0200

    plots hopefully more friendly to the colorblind

--- benchmark/plots.py
+++ benchmark/plots.py
@@ -2,10 +2,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser()
+"""parser = argparse.ArgumentParser()
 parser.add_argument("--algo", help="the algorithm to benchmark", required=True)
 args = parser.parse_args()
-assert args.algo in ["mul", "div"]
+assert args.algo in ["mul", "div"]"""
+class args:
+    algo = "mul"
 
 def regression(x : np.array, y : np.array):
     # linear for for y = m * x + b and r^2
@@ -20,11 +22,12 @@ def load(fname):
     n, t, m = array[:,0], array[:,1], array[:,2]
     return n, t, m
 
-def plotsub(ax, x, fx, y, color="orchid"):
+def plotsub(ax, x, fx, y, **kwargs):
     fy, r2 = regression(fx, y)
-    ax.plot(x, fy, '-', color=color)
+    ax.plot(x, fy, '-', **kwargs)
+    kwargs.pop("label")
+    ax.plot([],[], '-', **kwargs, label=f"$R^2 = {r2[0]:0.5f}$")
     ax.plot(x,  y, '.', color="black")
-    ax.annotate(f"$R^2 = {r2[0]:0.5f}$", (max(x)/2, 0), fontsize=14)
     ax.set_xticks(x[-4::])
     ax.set_yticks(y[-4::])
     ax.set_xticklabels([f"2^{j:.0f}" for j in np.log2(x[-4::])])
@@ -47,19 +50,27 @@ def plotmul():
     fig.set_size_inches(18, 6)
 
     if True: # time, fast fourier O(nlgn)
-        plotsub(ax1, x, x * np.log(x), y, color="orchid")
+        plotsub(ax1, x, x * np.log(x), y, color="cyan", linewidth=3, label='FFT')
         ax1.set_title(f"fft {subtitles[args.algo]}: " + r"$O(n \lg n)$", fontsize=16)
         ax1.set_ylabel('time (seconds)', fontsize=16)
         ax1.set_xlabel('input size', fontsize=16)
+        ax1.legend(prop={'size': 14})
 
     if True: # time, numpy O(n2) vs fast fouirer
-        ax2.plot(x,    y, '-', color="orchid")
-        plotsub(ax2, nx, nx * nx, ny, color="deepskyblue")
+        ax2.plot(x,    y, '-', color="cyan", linewidth=3, label='FFT')
+        ax2.plot(x,    y, '.', color="black")
+        plotsub(ax2, nx, nx * nx, ny, color="crimson", linewidth=3, label='NumPy')
         ax2.set_title(f"NumPy {subtitles[args.algo]}: " + r"$O(n^2)$ (vs fft)", fontsize=16)
+        ax2.legend(prop={'size': 14})
 
     if True: # memory, numpy vs fast fourier
-        ax3.plot(x,   z, '-', color="orchid")
-        ax3.plot(nx, nz, '-', color="deepskyblue")
+        ax3.plot(x,   z, '-', color="cyan",     linewidth=3, label='FFT')
+        ax3.plot(x,   z, '.', color="black")
+        ax3.plot(nx, nz, '-', color="crimson", linewidth=3, label='NumPy')
+        ax3.plot(nx,   nz, '.', color="black")
+
+        #ax3.plot([], [], color="orchid", label='Inline label 2')
+        ax3.legend(prop={'size': 14})
         ax3.set_title(f"NumPy vs fft", fontsize=14)
         ax3.set_xticks(x[-4::])
         ax3.set_yticks(z[-4::])
